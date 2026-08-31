e3f760f1910dc3bf004197ac97277e315c12bd3d
lovell06 <sontnx06@gmail.com>
2026-08-31 20:17:20 +0700

    register TimeProvider

--- src/Services/Identity/Application/DependencyInjection.cs
+++ src/Services/Identity/Application/DependencyInjection.cs
@@ -6,6 +6,8 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        services.AddSingleton(TimeProvider.System);
+
         services.AddMediatR(cfg =>
         {
             cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
