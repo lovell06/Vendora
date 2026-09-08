9be74eed68717e747555f2881f70dcb2fca21ea7
lovell06 <sontnx06@gmail.com>
2026-09-08 16:55:20 +0700

    insert Jwt bearer configuration

--- src/Services/Identity/Api/DependencyInjection.cs
+++ src/Services/Identity/Api/DependencyInjection.cs
@@ -1,9 +1,50 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Vendora.Services.Identity.Infrastructure.Options;
+
 namespace Vendora.Services.Identity.Api;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddApi(this IServiceCollection services)
-    {
+    public static IServiceCollection AddApi(this IServiceCollection services, IConfiguration configuration)
+    {        
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            var jwtOptions = configuration
+                .GetSection(JwtOptions.SectionName)
+                .Get<JwtOptions>() ?? throw new InvalidOperationException("Jwt is not configured.");
+
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = jwtOptions.Issuer,
+
+                ValidateAudience = true,
+                ValidAudience = jwtOptions.Audience,
+
+                ValidateLifetime = true,
+
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.Key))
+            };
+
+            options.Events = new JwtBearerEvents
+            {
+                OnAuthenticationFailed = context =>
+                {
+                    var logger = context.HttpContext.RequestServices.GetRequiredService<ILogger<JwtBearerHandler>>();
+                    logger.LogWarning(context.Exception, "JWT validation failed.");
+                    return Task.CompletedTask;
+                }
+            };
+        });
+
+        services.AddAuthorization();
         return services;
     }
 }
