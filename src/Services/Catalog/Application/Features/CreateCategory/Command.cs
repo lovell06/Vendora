a7b46c2704713a4bc84d000cc1975e224c052b5d
lovell06 <sontnx06@gmail.com>
2026-09-17 16:11:12 +0700

    delete features

--- src/Services/Catalog/Application/Features/CreateCategory/Command.cs
+++
@@ -1,8 +0,0 @@
-using Vendora.BuildingBlocks.Cqrs;
-
-namespace Vendora.Services.Catalog.Application.Features.CreateCategory;
-
-public sealed class Command : ICommand
-{
-    public required string Name { get; init; }
-}
